622652999bf80cdbc7925e51d272d898596a29c6
adwivedi008 <67369950+adwivedi008@users.noreply.github.com>
2020-06-25 21:13:48 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,70 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BooksFree4U/ABOUT</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <style type="text/css">
+               a.TitleLink
+               {
+                   display: run-in;
+                   width: auto;
+                   border: solid gray 1px;
+                   font-size: 15px;
+                   padding: 5px;
+                   text-decoration: none;
+               }
+               a.TitleLink:hover
+               {
+                   color: #234555;
+               }
+    </style>
+  </head>
+  <body>
+    <header>
+        <div class="container">
+        <div id="logo">
+            <a href="index.html"><img src="./images/premiumlogo/logo.png" height="130px" width="240px"></a>
+        </div>
+        <nav>
+          <ul >
+            <li><a href="index.php" class="TitleLink">HOME</a></li>
+            <li><a href="RecentbooksPost.php" target="_blank"class="TitleLink" >Recent Post(Books)</a></li>
+            <li><a href="courses.php" target="_blank"class="TitleLink" >COURSES</a></li>
+            <li><a href="about.php" target="_blank"class="TitleLink">ABOUT</a></li>
+            <li><a href="Requestabook.php" target="_blank" class="TitleLink">REQUEST_A_BOOK</a></li>
+            <li><a href="donateabook.php" target="_blank" class="TitleLink">DONATE_A_BOOK</a></li>
+          </ul>
+        </nav>
+        </div>
+        
+    </header>
+      <div alignment="center"><ul >
+              <li><form name="form1" method="post" action="searchresult.php">
+                <input name="search" type="text" size="80px"/>
+                <input type="submit" name="Submit" valur="search" action='submit form'/></li>
+          </ul></div>
+     <section id="showcase">
+        <div class="container">
+          <h1>Welcome to BookFree4U</h1>
+          <p>We are working hard to provide free e-books.
+            Booksfree for you is always available for you.
+            You can request a book by mailing us on bookhibook555@gmail.com</p>
+          <p>You can help us by donating a book on bookhibook555@gmail.com or
+             can help us by donating us on 8962294997(on phonepay,gpay,and paytm)
+           <h1><img src="bhimqr.png" width="300px" height="400px"></h1>
+           <p class="login"><a href="loginpage.php" target="blank">Login</a></p>
+         </p>
+        </div>
+        </section>
+        <div class="dropdown">
+         <button class="dropbtn">COURSES</button>
+         <div class="dropdown-content">
+           <a href="#">Link 1</a>
+           <a href="#">Link 2</a>
+           <a href="#">Link 3</a>
+         </div>
+            
+        </div>
+          
+  </body>
+</html>
